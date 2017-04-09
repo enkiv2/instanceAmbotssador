5ecab0c5cc935a698f6bdaa8df204b052e477426
enkiv2 <john.ohno@gmail.com>
2017-04-09 17:36:18 -0400

    fix some bugs

--- register.py
+++ register.py
@@ -6,14 +6,14 @@ if(len(sys.argv)<=4):
 	print("Usage: register clientName instanceUrl email password")
 	sys.exit(1)
 
-(clientName, instanceUrl, email, password) = sys.argv
+(clientName, instanceUrl, email, password) = sys.argv[1:]
 
-clientCredFname="_".join([clientName, instanceUrl, "clientcred.txt"])
-userCredFname="_".join([clientName, instanceUrl, email, "clientcred.txt"])
+clientCredFname="_".join([clientName, instanceUrl.replace("https://", "").replace("/", ""), "clientcred.txt"])
+userCredFname="_".join([clientName, instanceUrl.replace("https://", "").replace("/", ""), email, "clientcred.txt"])
 
 if not (os.path.exists(clientCredFname)):
 	Mastodon.create_app(clientName, to_file=clientCredFname, api_base_url=instanceUrl)
 
-mastodon = Mastodon(client_id=clientCredFname)
+mastodon = Mastodon(client_id=clientCredFname, api_base_url=instanceUrl)
 mastodon.log_in(email, password, to_file=userCredFname)
 
