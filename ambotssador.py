2de8c8438d3b131fc6ed37ecfa5679c13311fe75
enkiv2 <john.ohno@gmail.com>
2017-04-10 16:12:49 -0400

    retry logic

--- ambotssador.py
+++ ambotssador.py
@@ -37,61 +37,68 @@ maxRank=[0]*endpointCount
 
 iterations=0
 while True:
-	start=time.time()
-	tl=[]
-	for i in range(0, endpointCount):
-		tl.append(mastodon[i].timeline_local(limit=1000))
+	try:
+		start=time.time()
+		tl=[]
+		for i in range(0, endpointCount):
+			tl.append(mastodon[i].timeline_local(limit=1000))
 
-	tlS=[{}]*endpointCount
-	count=0
-	favFreq=0
-	for i in range(0, endpointCount):
-		for toot in tl[i]:
-			favs=toot["favourites_count"]
-			tootId=toot["id"]
-			if favs in tlS[i]:
-				tlS[i][favs].append(tootId)
-			else:
-				tlS[i][favs]=[tootId]
-		fk=tlS[i].keys()
-		fk.sort()
-		fk.reverse()
-		done=False
-		if(fk[0]>maxRank[i]):
-			maxRank[i]=fk[0]
-		elif(iterations%10 == 0): # decay threshhold
-			maxRank[i]-=1
-		for rank in fk:
-			if(rank==0): # don't bother boosting anything with zero favs
-				break
-			if(done):
-				break
-			if(maxRank[i]-rank>5):
-				done=True
-			for tootId in tlS[i][rank]:
-				startCount=count
-				if not (tootId in boostedToots):
-					for other in range(0, endpointCount):
-						if other != i:
-							try:
-								mastodon[other].status_reblog(tootId)
-								time.sleep(1)
-								done=True
-							except:
-								pass
-					favFreq=rank; count+=1
-					boostedToots.append(tootId)
-					boostedToots=list(set(boostedToots))
-					pickle.dump(boostedToots, open("boostedToots.pickle", "w"))
-					time.sleep(1)
-				print("Boosted "+str(count-startCount)+" toots from node "+sys.argv[3+(i*3)]+" -- fav freq="+str(rank))
+		tlS=[{}]*endpointCount
+		count=0
+		favFreq=0
+		for i in range(0, endpointCount):
+			for toot in tl[i]:
+				favs=toot["favourites_count"]
+				tootId=toot["id"]
+				if favs in tlS[i]:
+					tlS[i][favs].append(tootId)
+				else:
+					tlS[i][favs]=[tootId]
+			fk=tlS[i].keys()
+			fk.sort()
+			fk.reverse()
+			done=False
+			if(fk[0]>maxRank[i]):
+				maxRank[i]=fk[0]
+			elif(iterations%10 == 0): # decay threshhold
+				maxRank[i]-=1
+			for rank in fk:
+				if(rank==0): # don't bother boosting anything with zero favs
+					break
+				if(done):
+					break
+				if(maxRank[i]-rank>5):
+					done=True
+				for tootId in tlS[i][rank]:
+					startCount=count
+					if not (tootId in boostedToots):
+						for other in range(0, endpointCount):
+							if other != i:
+								try:
+									mastodon[other].status_reblog(tootId)
+									time.sleep(1)
+									done=True
+								except:
+									pass
+						favFreq=rank; count+=1
+						boostedToots.append(tootId)
+						boostedToots=list(set(boostedToots))
+						pickle.dump(boostedToots, open("boostedToots.pickle", "w"))
+						time.sleep(1)
+					print("Boosted "+str(count-startCount)+" toots from node "+sys.argv[3+(i*3)]+" -- fav freq="+str(rank))
 
-	pickle.dump(boostedToots, open("boostedToots.pickle", "w"))
-	print("Boosted "+str(count)+" toots -- fav freq "+str(favFreq))
-	iterations+=1
-	end=time.time()
-	delta=end-start
-	if(delta<rate):
-		time.sleep(rate-delta)
+		pickle.dump(boostedToots, open("boostedToots.pickle", "w"))
+		print("Boosted "+str(count)+" toots -- fav freq "+str(favFreq))
+		iterations+=1
+		end=time.time()
+		delta=end-start
+		if(delta<rate):
+			time.sleep(rate-delta)
+	except:
+		# If we have an error in getting timeline, wait 60 seconds & then reconnect to all instances
+		time.sleep(60)
+		mastodon=[]
+		for i in range(0, endpointCount):
+			mastodon.append(Mastodon(sys.argv[1+(3*i)], access_token=sys.argv[2+(3*i)], api_base_url=sys.argv[3+(3*i)]))
 
 
