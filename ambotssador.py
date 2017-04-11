34d17d1a9a87cdd4b120e4f6e26844563772c4ac
enkiv2 <john.ohno@gmail.com>
2017-04-11 16:13:59 -0400

    heavily attenuate post frequency

--- ambotssador.py
+++ ambotssador.py
@@ -13,6 +13,8 @@ try:
 except:
 	import pickle
 
+from random import Random
+random=Random()
 
 from mastodon import Mastodon
 
@@ -78,12 +80,40 @@ while True:
 					break
 				if(maxRank[i]-rank>5):
 					done=True
-				for tootId in tlS[i][rank]:
-					startCount=count
-					if not (tootId in boostedToots):
-						for other in range(0, endpointCount):
-							if other != i:
+				tootId=random.choice(tlS[i][rank])
+				startCount=count
+				if not (tootId in boostedToots):
+					other=random.choice(range(0, endpointCount))
+					if other != i:
+						try:
+							msgbase="\n(Boosting from "+sys.argv[3+(3*i)]+": "+tootId+", with "+str(rank)+" favorites)"
+							user_components=tootId.split("/")
+							username="@".join([user_components[3], user_components[2]])
+							orig=username+" "+tootInfo[tootId]["content"].replace("<p>", "\n").replace("</p>", "\n")
+							print(orig)
+							if(len(orig)>500-len(msgbase)):
+								orig=orig[:500-len(msgbase)-4]+"..."
+							print(orig+msgbase)
+							mastodon[other].toot(orig+msgbase)
+							time.sleep(60)
+							done=True
+						except Exception as e:
+							print("Error posting to "+sys.argv[3+(other*3)]+":")
+							print(e)
+							traceback.print_exc()
+							retrySuccess=False
+							for retries in range(0, 3):
+								if(retrySuccess):
+									break
 								try:
+									print("Reconnecting to "+sys.argv[3+(other*3)])
+									mastodon[other]=Mastodon(
+										client_id=sys.argv[1+(3*other)],
+										access_token=sys.argv[2+(3*other)],
+										api_base_url=sys.argv[3+(3*other)])
+									print("Reconnected")
+									time.sleep(1+retries*10)
+									print("Retrying post")
 									msgbase="\n(Boosting from "+sys.argv[3+(3*i)]+": "+tootId+", with "+str(rank)+" favorites)"
 									user_components=tootId.split("/")
 									username="@".join([user_components[3], user_components[2]])
@@ -93,57 +123,29 @@ while True:
 										orig=orig[:500-len(msgbase)-4]+"..."
 									print(orig+msgbase)
 									mastodon[other].toot(orig+msgbase)
-									time.sleep(10)
+									time.sleep(60)
+									retrySuccess=True
 									done=True
-								except Exception as e:
-									print("Error posting to "+sys.argv[3+(other*3)]+":")
-									print(e)
+								except Exception as f:
+									print(f)
 									traceback.print_exc()
-									retrySuccess=False
-									for retries in range(0, 3):
-										if(retrySuccess):
-											break
-										try:
-											print("Reconnecting to "+sys.argv[3+(other*3)])
-											mastodon[other]=Mastodon(
-												client_id=sys.argv[1+(3*other)],
-												access_token=sys.argv[2+(3*other)],
-												api_base_url=sys.argv[3+(3*other)])
-											print("Reconnected")
-											time.sleep(1+retries*10)
-											print("Retrying post")
-											msgbase="\n(Boosting from "+sys.argv[3+(3*i)]+": "+tootId+", with "+str(rank)+" favorites)"
-											user_components=tootId.split("/")
-											username="@".join([user_components[3], user_components[2]])
-											orig=username+" "+tootInfo[tootId]["content"].replace("<p>", "\n").replace("</p>", "\n")
-											print(orig)
-											if(len(orig)>500-len(msgbase)):
-												orig=orig[:500-len(msgbase)-4]+"..."
-											print(orig+msgbase)
-											mastodon[other].toot(orig+msgbase)
-											time.sleep(10)
-											retrySuccess=True
-											done=True
-										except Exception as f:
-											print(f)
-											traceback.print_exc()
-											time.sleep(1+retries*10)
-									if not retrySuccess:
-										print("Failed; removing from list")
-										mastodon=mastodon[:other]+mastodon[other+1:]
-										tl=tl[:other]+tl[other+1:]
-										tlS=tlS[:other]+tlS[other+1:]
-										sys.argv=sys.argv[:3*other+1]+sys.argv[3*other+4:]
-										endpointCount-=1
-										break
-									
-						favFreq=rank; count+=1
-						boostedToots.append(tootId)
-						boostedToots=list(set(boostedToots))
-						pickle.dump(boostedToots, open("boostedToots.pickle", "w"))
-						time.sleep(10)
-					if(count>startCount):
-						print("Boosted "+str(count-startCount)+" toots from node "+sys.argv[3+(i*3)]+" -- fav freq="+str(rank))
+									time.sleep(1+retries*10)
+							if not retrySuccess:
+								print("Failed; removing from list")
+								mastodon=mastodon[:other]+mastodon[other+1:]
+								tl=tl[:other]+tl[other+1:]
+								tlS=tlS[:other]+tlS[other+1:]
+								sys.argv=sys.argv[:3*other+1]+sys.argv[3*other+4:]
+								endpointCount-=1
+								break
+							
+					favFreq=rank; count+=1
+					boostedToots.append(tootId)
+					boostedToots=list(set(boostedToots))
+					pickle.dump(boostedToots, open("boostedToots.pickle", "w"))
+					time.sleep(60)
+				if(count>startCount):
+					print("Boosted "+str(count-startCount)+" toots from node "+sys.argv[3+(i*3)]+" -- fav freq="+str(rank))
 
 		if(count>0):
 			pickle.dump(boostedToots, open("boostedToots.pickle", "w"))
